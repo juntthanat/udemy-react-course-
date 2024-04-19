5d2357dafc5cdb6b55d0818e272a7c0c67e1d2f9
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-04-19 13:13:59 +0700

    feat: make first dynamic page

--- section1/01-starting-project/src/App.jsx
+++ section1/01-starting-project/src/App.jsx
@@ -1,14 +1,27 @@
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max){
+  return Math.floor(Math.random() * (max));
+}
+function MyHeader() {
+
+
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {reactDescriptions[genRandomInt(reactDescriptions.length)]} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <MyHeader />
       <main>
         <h2>Time to get started!</h2>
       </main>
