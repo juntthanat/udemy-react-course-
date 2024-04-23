4d8aa9bce10f55ea0b5c48987c9e36c3db7f4b64
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-04-23 18:11:29 +0700

    rendered content conditionally

--- section1/01-starting-project/src/App.jsx
+++ section1/01-starting-project/src/App.jsx
@@ -1,12 +1,13 @@
-import {CORE_CONCEPTS} from "./data.js"
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 import MyHeader from "./components/MyHeader/MyHeader.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
-
+import { useState } from "react";
 
 function App() {
-  function handleClick(event){
-    console.log(event)
+  const [selectedTopic, setSelectedTopic] = useState();
+  function handleClick(event) {
+    setSelectedTopic(event);
   }
   return (
     <div>
@@ -17,26 +18,34 @@ function App() {
           <ul>
             <CoreConcept
               title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0]. description}
+              description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept {...CORE_CONCEPTS[1]}
-            />
-            <CoreConcept {...CORE_CONCEPTS[2]}
-            />
-            <CoreConcept {...CORE_CONCEPTS[3]}
-            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onClick={() => handleClick("component")}>Components</TabButton>
+            <TabButton onClick={() => handleClick("components")}>
+              Components
+            </TabButton>
             <TabButton onClick={() => handleClick("jsx")}>JSX</TabButton>
             <TabButton onClick={() => handleClick("props")}>Props</TabButton>
             <TabButton onClick={() => handleClick("state")}>State</TabButton>
           </menu>
-          Dynamic Content
+          {/* {!selectedTopic && <p>Please Select a Topic</p>} */}
+          {selectedTopic ? (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          ) : <p>Please Select a Topic</p>}
         </section>
       </main>
     </div>
