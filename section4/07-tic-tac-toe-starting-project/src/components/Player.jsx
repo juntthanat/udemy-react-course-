deac5f7880c0955859f166f913c9b59524b06061
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-04-24 20:52:41 +0700

    feat: two way binding

--- section4/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ section4/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,22 +1,31 @@
 import { useState } from "react";
-export default function PLayer({ name, symbol }) {
+export default function PLayer({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
 
   const handleEditClick = () => {
     setIsEditing((editing) => !editing);
   };
+  const handleChange = (event) => {
+    setPlayerName(event.target.value);
+  };
 
-  let playerName =
+  let editablePlayerName =
     isEditing === false ? (
-      <span className="player-name">{name}</span>
+      <span className="player-name">{playerName}</span>
     ) : (
-      <input type="text" required value={name}></input>
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleChange}
+      ></input>
     );
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
