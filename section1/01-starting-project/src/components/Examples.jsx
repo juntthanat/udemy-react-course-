ed6b0bfb98edb827b6888fd2a552a415d2129aaa
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-04-24 20:07:45 +0700

    feat: small fix

--- section1/01-starting-project/src/components/Examples.jsx
+++ section1/01-starting-project/src/components/Examples.jsx
@@ -12,7 +12,6 @@ export default function Examples() {
   return (
     <Section title="Examples" id="examples">
       <Tabs
-      buttonsContainer="menu"
         buttons={
           <>
             <TabButton
