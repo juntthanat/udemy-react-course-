3c8225962e2b056c879feb64776174c536495610
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-04-25 15:22:52 +0700

    feat: deriving state from props

--- section4/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ section4/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -6,19 +6,27 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({onSelectSquare, turns}) {
+    let gameBoard = initialGameBoard;
+    
+    for(const turn of turns) {
+        const {square, player} = turn;
+        const {row, col} = square;
 
-  const handleSelectSquare = (rowIndex, colIndex) => {
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
-    onSelectSquare();
-  };
+        gameBoard[row][col] = player;
+    }
+//   const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+//   const handleSelectSquare = (rowIndex, colIndex) => {
+//     setGameBoard((prevGameBoard) => {
+//       const updatedBoard = [
+//         ...prevGameBoard.map((innerArray) => [...innerArray]),
+//       ];
+//       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+//       return updatedBoard;
+//     });
+//     onSelectSquare();
+//   };
 
   return (
     <ol id="game-board">
@@ -27,7 +35,7 @@ export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
