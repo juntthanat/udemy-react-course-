918b62b9bde8f181de8de89514f7bdd97bdc6dae
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-04-26 14:19:16 +0700

    feat: reducing State Management & Identifying Unnecessary State

--- section4/07-tic-tac-toe-starting-project/src/App.jsx
+++ section4/07-tic-tac-toe-starting-project/src/App.jsx
@@ -2,14 +2,23 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import { useState } from "react";
+
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = 'X';
+  if(gameTurns.length > 0 && gameTurns[0].player === 'X'){
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   const handleSelectSquare = (rowIndex, colIndex) => {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
+      let currentPlayer = deriveActivePlayer(prevTurns);
       if (prevTurns.length > 0 && prevTurns[0].player === "X") {
         currentPlayer = "O";
       }
