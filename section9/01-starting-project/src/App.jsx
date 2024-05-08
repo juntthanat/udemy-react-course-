3d381c48db45b73c6ae42e55c1211019905845b3
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-05-08 16:01:51 +0700

    feat: tiny fix in design

--- section9/01-starting-project/src/App.jsx
+++ section9/01-starting-project/src/App.jsx
@@ -3,12 +3,12 @@ import ProjectBar from "./components/ProjectBar";
 function App() {
   return (
     <main className="w-screen h-screen flex flex-col">
-      <div className="w-full flex-auto h-4"/>
-      <section className="w-full flex-auto h-5/6">
+      <div className="w-full h-12"/>
+      <section className="w-full h-5/6 flex flex-auto flex-row">
         <ProjectBar></ProjectBar>
         MainPage
       </section>
-      <div className="w-full flex-auto h-2 bg-stone-700"/>
+      {/* <div className="w-full flex-auto h-2 bg-stone-700"/> */}
     </main>
   );
 }
