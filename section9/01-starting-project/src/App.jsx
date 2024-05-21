f6bb13a19bbec8561140abc95f0af637a2a9d376
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-05-22 03:05:07 +0700

    feat: home page unselect design

--- section9/01-starting-project/src/App.jsx
+++ section9/01-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import ProjectBar from "./components/ProjectBar";
+import HomePageUnselect from "./components/HomePageUnselect";
 
 function App() {
   return (
@@ -6,7 +7,9 @@ function App() {
       <div className="w-full h-12"/>
       <section className="w-full h-5/6 flex flex-auto flex-row">
         <ProjectBar></ProjectBar>
-        MainPage
+        <section className="w-3/4 h-full">
+          <HomePageUnselect/>
+        </section>  
       </section>
       {/* <div className="w-full flex-auto h-2 bg-stone-700"/> */}
     </main>
--- section9/01-starting-project/src/components/ProjectBar.jsx
+++ section9/01-starting-project/src/components/ProjectBar.jsx
@@ -1,10 +1,12 @@
-
-
-export default function ProjectBar(){
-
-    return (
-        <section className="w-1/4 h-full rounded-tr-3xl bg-stone-900">
-            hello world
-        </section>
-    )
+export default function ProjectBar() {
+  return (
+    <section className="w-1/4 h-full rounded-tr-3xl bg-stone-900 pt-20 pl-12">
+      <p className="text-white text-3xl font-semibold">
+        YOUR PROJECTS
+      </p>
+      <button className="text-gray-400 bg-stone-700 h-10 w-36 rounded mt-12">
+        + Add Project
+      </button>
+    </section>
+  );
 }
