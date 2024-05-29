7921420b922b6cf3d41dfc8a81f18eb5a9536d17
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-05-29 09:49:47 +0700

    feat: addpage component without css and javascript

--- section9/01-starting-project/src/components/AddPage.jsx
+++ section9/01-starting-project/src/components/AddPage.jsx
@@ -1,9 +1,17 @@
+import SaveButton from "./SaveButton"
+
 
 export default function AddPage() {
 
     return (
-        <section>
-            Add Page
-        </section>
+        <div className="w-full h-full flex items-center pt-40 pr-64 pl-12 flex-col ">
+            <SaveButton/>
+            Title 
+            <input></input>
+            Description
+            <input></input>
+            Due Date 
+            <input></input>
+        </div>
     )
 }
---
+++ section9/01-starting-project/src/components/SaveButton.jsx
@@ -0,0 +1,10 @@
+
+export default function SaveButton() {
+
+    return (
+        <div className="w-full bg-red-100 flex justify-end">
+            <button>Cancel</button>
+            <button>Save</button>
+        </div>
+    )
+}
