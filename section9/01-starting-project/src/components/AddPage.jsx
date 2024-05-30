fc7d026c850129dae2004aa5e0a233b73544778d
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-05-30 13:12:41 +0700

    feat: add input

--- section9/01-starting-project/src/components/AddPage.jsx
+++ section9/01-starting-project/src/components/AddPage.jsx
@@ -7,11 +7,11 @@ export default function AddPage() {
         <div className="w-full h-full flex items-center pt-40 pr-64 pl-12 flex-col ">
             <SaveButton/>
             Title 
-            <input></input>
+            <input className="w-full h-8 bg-stone-200 outline-none border-gray-300 focus:border-stone-700 border-b-2 "></input>
             Description
-            <input></input>
+            <input className="w-full h-8 bg-stone-200 outline-none border-gray-300 focus:border-stone-700 border-b-2 "></input>
             Due Date 
-            <input></input>
+            <input className="w-full h-8 bg-stone-200 outline-none border-gray-300 focus:border-stone-700 border-b-2 "></input>
         </div>
     )
 }
--- section9/01-starting-project/src/components/SaveButton.jsx
+++ section9/01-starting-project/src/components/SaveButton.jsx
@@ -3,8 +3,8 @@ export default function SaveButton() {
 
     return (
         <div className="w-full bg-red-100 flex justify-end">
-            <button>Cancel</button>
-            <button>Save</button>
+            <button className="w-24 h-10  bg-white rounded">Cancel</button>
+            <button className="w-24 h-10  bg-stone-900 text-white rounded">Save</button>
         </div>
     )
 }
