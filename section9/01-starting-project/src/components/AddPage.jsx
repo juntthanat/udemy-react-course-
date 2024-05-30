41c10f6976171f787e277de628a4abf211b57314
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-05-30 13:39:28 +0700

    feat: input field and css

--- section9/01-starting-project/src/components/AddPage.jsx
+++ section9/01-starting-project/src/components/AddPage.jsx
@@ -1,17 +1,32 @@
-import SaveButton from "./SaveButton"
-
+import SaveButton from "./SaveButton";
 
 export default function AddPage() {
-
-    return (
-        <div className="w-full h-full flex items-center pt-40 pr-64 pl-12 flex-col ">
-            <SaveButton/>
-            Title 
-            <input className="w-full h-8 bg-stone-200 outline-none border-gray-300 focus:border-stone-700 border-b-2 "></input>
-            Description
-            <input className="w-full h-8 bg-stone-200 outline-none border-gray-300 focus:border-stone-700 border-b-2 "></input>
-            Due Date 
-            <input className="w-full h-8 bg-stone-200 outline-none border-gray-300 focus:border-stone-700 border-b-2 "></input>
+  return (
+    <div className="w-full h-full flex items-center pt-40 pr-64 pl-12 flex-col ">
+      <SaveButton />
+      <div className="w-full h-full font-bold flex flex-col space-y-6 mt-6">
+        <div>
+          TITLE
+          <input
+            type="text"
+            className="w-full h-10 bg-stone-200 outline-none border-gray-300 focus:border-stone-700 border-b-2 p-2"
+          ></input>
         </div>
-    )
+        <div>
+          DESCRIPTION
+          <textarea
+            type="text"
+            className="w-full h-20 bg-stone-200 outline-none border-gray-300 focus:border-stone-700 border-b-2 p-2"
+          ></textarea>
+        </div>
+        <div>
+          DUE DATE
+          <input
+            type="date"
+            className="font-normal w-full h-10 bg-stone-200 outline-none border-gray-300 focus:border-stone-700 border-b-2 p-2"
+          ></input>
+        </div>
+      </div>
+    </div>
+  );
 }
--- section9/01-starting-project/src/components/SaveButton.jsx
+++ section9/01-starting-project/src/components/SaveButton.jsx
@@ -2,8 +2,8 @@
 export default function SaveButton() {
 
     return (
-        <div className="w-full bg-red-100 flex justify-end">
-            <button className="w-24 h-10  bg-white rounded">Cancel</button>
+        <div className="w-full flex justify-end">
+            <button className="w-24 h-10  rounded">Cancel</button>
             <button className="w-24 h-10  bg-stone-900 text-white rounded">Save</button>
         </div>
     )
