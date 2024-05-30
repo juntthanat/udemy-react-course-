b47c5ab43c46980d94ee26f28d6913750d396224
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-05-30 16:34:58 +0700

    feat: addPage minimum feature

--- section9/01-starting-project/src/components/SaveButton.jsx
+++ section9/01-starting-project/src/components/SaveButton.jsx
@@ -1,10 +1,10 @@
 
-export default function SaveButton() {
+export default function SaveButton({onSave, onCancel}) {
 
     return (
         <div className="w-full flex justify-end">
-            <button className="w-24 h-10  rounded">Cancel</button>
-            <button className="w-24 h-10  bg-stone-900 text-white rounded">Save</button>
+            <button onClick={onCancel}className="w-24 h-10  rounded">Cancel</button>
+            <button onClick={onSave} className="w-24 h-10  bg-stone-900 text-white rounded">Save</button>
         </div>
     )
 }
