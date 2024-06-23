49a7037cd4aea2fce2d379c08b45be55805e5b29
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-06-23 16:03:29 +0700

    feat: Handling Project Creation & Updating the UI

--- section9/01-starting-project-tutorial/src/components/NoProjectSelected.jsx
+++ section9/01-starting-project-tutorial/src/components/NoProjectSelected.jsx
@@ -1,14 +1,22 @@
-import NoProjectImage from "../assets/no-projects.png"
+import NoProjectImage from "../assets/no-projects.png";
 import Button from "./Button.jsx";
 
-export default function NoProjectSelected() {
+export default function NoProjectSelected({ onStartAddProject }) {
   return (
     <div className="mt-24 text-center w-2/3 ">
-      <img src={NoProjectImage} alt="An empty task list" className="x-16 h-16 object-contain mx-auto"/>
-      <h2 className="text-sl font-bold text-stone-500 my-4">No Project Selected</h2>
-      <p className="text-stone-400 mb-4">Selected a Project or get started with a new one</p>
+      <img
+        src={NoProjectImage}
+        alt="An empty task list"
+        className="x-16 h-16 object-contain mx-auto"
+      />
+      <h2 className="text-sl font-bold text-stone-500 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 mb-4">
+        Selected a Project or get started with a new one
+      </p>
       <p className="mt-8">
-        <Button>Create new project</Button>
+        <Button onClick={onStartAddProject}>Create new project</Button>
       </p>
     </div>
   );
