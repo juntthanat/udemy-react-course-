73d206ed54e5d4bf442a241f7378312084bc4c97
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-07-08 14:28:19 +0700

    feat: Handling project deletion

--- section9/01-starting-project-tutorial/src/App.jsx
+++ section9/01-starting-project-tutorial/src/App.jsx
@@ -33,6 +33,18 @@ function App() {
     });
   }
 
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
+
   function handleCancelAddProject() {
     setProjectsState((prevState) => {
       return {
@@ -51,9 +63,13 @@ function App() {
     });
   }
 
-  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
 
-  let content = <SelectedProject project={selectedProject}/>;
+  let content = (
+    <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+  );
   if (projectsState.selectedProjectId === null) {
     content = (
       <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
@@ -67,7 +83,6 @@ function App() {
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
         onSelectProject={handleSelectProject}
-
       />
       {content}
     </main>
