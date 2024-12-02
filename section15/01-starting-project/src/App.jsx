6c3f184e948a9166eee09e13349c59867a93bf2f
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-12-02 17:06:01 +0700

    feat: practice fetching

--- section15/01-starting-project/src/App.jsx
+++ section15/01-starting-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useCallback } from "react";
+import { useRef, useState, useCallback, useEffect } from "react";
 
 import Places from "./components/Places.jsx";
 import Modal from "./components/Modal.jsx";
@@ -7,6 +7,7 @@ import logoImg from "./assets/logo.png";
 import AvailablePlaces from "./components/AvailablePlaces.jsx";
 import { updateUserPlaces } from "./http.js";
 import Error from "./components/Error.jsx";
+import { fetchUserPlaces } from "./http.js";
 
 function App() {
   const selectedPlace = useRef();
@@ -14,7 +15,24 @@ function App() {
   const [userPlaces, setUserPlaces] = useState([]);
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const places = await fetchUserPlaces();
+        setUserPlaces(places);
+      } catch (error) {
+        setError({ message: error.message || "failed to fetch user places" });
+      }
+      setIsFetching(false);
+    }
+    fetchPlaces();
+  }, []);
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -39,7 +57,6 @@ function App() {
     try {
       await updateUserPlaces([selectedPlace, ...userPlaces]);
     } catch (error) {
-      // Occured because cor policy from localhost:5173 (vite) to localhost:3000
       setUserPlaces(userPlaces);
       setErrorUpdatingPlaces({ message: error.message || "Failed to update" });
     }
@@ -100,12 +117,17 @@ function App() {
         </p>
       </header>
       <main>
-        <Places
-          title="I'd like to visit ..."
-          fallbackText="Select the places you would like to visit below."
-          places={userPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
+        {error && <Error title="An error Occured" message={error.message} />}
+        {!error && (
+          <Places
+            title="I'd like to visit ..."
+            fallbackText="Select the places you would like to visit below."
+            isLoading={isFetching}
+            loadingText="Fetching your places..."
+            places={userPlaces}
+            onSelectPlace={handleStartRemovePlace}
+          />
+        )}
 
         <AvailablePlaces onSelectPlace={handleSelectPlace} />
       </main>
--- section15/01-starting-project/src/http.js
+++ section15/01-starting-project/src/http.js
@@ -15,7 +15,7 @@ export async function updateUserPlaces(places) {
     method: "PUT",
     body: JSON.stringify({ places }),
     headers: {
-      "Content-Tyoe": "application/json",
+      "Content-Type": "application/json",
     },
   });
 
@@ -27,3 +27,15 @@ export async function updateUserPlaces(places) {
 
   return resData.message;
 }
+
+export async function fetchUserPlaces() {
+    const response = await fetch("http://localhost:3000/user-places");
+    const resData = await response.json();
+  
+    // 200 or 300 status code if false 400 or 500 status code
+    if (!response.ok) {
+      throw new Error("Failed to fetch user places");
+    }
+  
+    return resData.places;
+  }
