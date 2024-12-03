ae87f1a025d3822e3f74273164866ab493507f30
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-12-03 16:01:27 +0700

    Lesson: Validating Input Via Built-In Validating Props

--- section17/01-starting-project/src/components/Login.jsx
+++ section17/01-starting-project/src/components/Login.jsx
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
@@ -10,11 +12,15 @@ export default function Login() {
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
 
-    console.log(enteredEmail, enteredPassword)
+    console.log(enteredEmail, enteredPassword);
 
-    
+    const emailIsValid = enteredEmail.includes("@");
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
   }
-
 
   return (
     <form>
@@ -23,22 +29,15 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            ref={email}
-          />
+          <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Plese Enter a Valid Email Address</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            ref={password}
-          />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
