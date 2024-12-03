3ecfc12300016082b7e4aee053cffab3ef4f1d3a
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-12-03 16:15:07 +0700

    Lesson: Building & Using a Reusable Input Component

--- section17/01-starting-project/src/components/Signup.jsx
+++ section17/01-starting-project/src/components/Signup.jsx
@@ -1,4 +1,7 @@
 export default function Signup() {
+
+  const [passwordsAreNotEqual, setPasswordAreNotEqual] = useState(false);
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -6,6 +9,12 @@ export default function Signup() {
     const acquisitionChannel = fd.getAll("acquisition");
     const data = Object.fromEntries(fd.entries());
     data.acquisition = acquisitionChannel;
+
+    if(data.password !== data('confirm-password')){
+      setPasswordAreNotEqual(true);
+      return
+    }
+
     console.log(data);
   }
 
@@ -33,6 +42,7 @@ export default function Signup() {
             name="confirm-password"
             required
           />
+          <div className="control-error">{passwordsAreNotEqual && <p>Passwords must match.</p>}</div>
         </div>
       </div>
 
